9b1fce7c9d8f7f353d9a8cf181827534c78d9e38
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-04-13 14:47:53 -0500

    Add files via upload

---
+++ stack_vs_queues.js
@@ -0,0 +1,23 @@
+// Stacks
+
+//google
+//udemy.com
+//youtube
+
+//Arrays
+//Linked Lists
+
+//Queues 
+// 0.       1.       2.        3.  this is an O(n) operations
+// Matt -- Joy -- Samir -- Pavel
+// Head                    Tail    this is an O(1) operations 
+// Matt -- Joy -- Samir -- Pavel
+
+//Arrays
+//Linked Lists
+
+
+twitter
+youtube
+udemy.com
+google
